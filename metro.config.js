3f98cfd70da4ea1ceeb071d410977ed037e962de
VAMSIKRISHNA BATRAJU <22481A6011@gecgudlavallerumic.in>
2026-02-17 21:20:35 +0530

    Update metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,3 @@
-const { getDefaultConfig } = require("@expo/metro-config");
+const { getDefaultConfig } = require('@expo/metro-config');
 
-const config = getDefaultConfig(__dirname);
-
-module.exports = config;
+module.exports = getDefaultConfig(__dirname);
